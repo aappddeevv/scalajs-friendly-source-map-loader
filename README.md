e4063c9229695effc80b5c60fb193a4d7f5994a8
aappddeevv <aappddeevv@gmail.com>
2020-03-12 19:26:42 -0400

    Added remote fetch caching.

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
 Drop in replacement for source-map-loader but this loader understands `file://`
 and `http[s]://` prefixes in source map files like the kind you find in scala.js
-generated source maps.
+generated source maps. It is often recommended that scala.js resources
+use the `-P:scalajs:mapSourceURI` option to set their source maps to
+github resources since there is no stable mechanism to include them in jars.
 
 Including scala sources increases the map size so use `bundleHttp: false` to not
 bundle them. They are bundled by default. Warnings that "file://" resources
 cannot be found can be turned off using `skipFileURLWarnings` which is true by
 default. If you depend on other scala.js libraries, they may have `file://`
 links embedded that cause warnings. If you work offline and do not have access
-to http resources (this loader does not cache http resources), then set your
+to http resources, then set your
 `bundleHttp` option using a nodejs environment variable: `bundleHttp:
-process.env.OFFLINE` then set the environment variable `OFFLINE="true"` before
+process.env.OFFLINE` and set the environment variable `OFFLINE="true"` before
 you run webpack. Note that "true" is in quotes to make at a string environment
 variable.
+
+Fetched resources are cached. The default cache directory is `<cwd>/.scala-js-sources`.
+Webpack already caches source maps so this cache mostly helps with 
+cold starts.
 
 As a drop-in replacement:
 ```javascript
@@ -26,7 +32,9 @@ As a drop-in replacement:
                     loader: "scalajs-friendly-source-map-loader",
                     options: {
                         skipFileURLWarnings: true, // or false, default is true
-                        bundleHttp: true // or false, default is true
+                        bundleHttp: true // or false, default is true,
+			cachePath: ".scala-js-sources", // cache dir name
+			noisyCache: false // whether cache changes are output
                     }
                 }],
                 enforce: "pre",
--- index.js
+++ index.js
@@ -11,34 +11,46 @@ var path = require("path");
 var async = require("async");
 var loaderUtils = require("loader-utils");
 const fetch = require("node-fetch")
+var url_parse = require("url").parse;
+
+const defaultCachePath = ".scala-js-sources"
 
 // Matches only the last occurrence of sourceMappingURL
 var baseRegex = "\\s*[@#]\\s*sourceMappingURL\\s*=\\s*([^\\s]*)(?![\\S\\s]*sourceMappingURL)",
 	// Matches /* ... */ comments
-	regex1 = new RegExp("/\\*"+baseRegex+"\\s*\\*/"),
+	regex1 = new RegExp("/\\*" + baseRegex + "\\s*\\*/"),
 	// Matches // .... comments
-	regex2 = new RegExp("//"+baseRegex+"($|\n|\r\n?)"),
+	regex2 = new RegExp("//" + baseRegex + "($|\n|\r\n?)"),
 	// Matches DataUrls
 	regexDataUrl = /data:[^;\n]+(?:;charset=[^;\n]+)?;base64,([a-zA-Z0-9+/]+={0,2})/;
 
-module.exports = function(input, inputMap) {
-    this.cacheable && this.cacheable();
-    var options = Object.assign(
-        {
-            skipFileURLWarnings: true,
-            bundleHttp: true,
-        },
-        loaderUtils.getOptions(this)
-    )
+// when run, loader function have been augmented with multiple helpers accessible
+// by this.<helper>, e.g., this.async(), this.callback, this.resolve, this.addDependency
+module.exports = function (input, inputMap) {
+	this.cacheable && this.cacheable();
+	var options = Object.assign(
+		{
+			// Do not issues warnings
+			skipFileURLWarnings: true,
+			// whether to fetch http documents, false means no fetch, only cache check
+			bundleHttp: true,
+			// where fetch resuts are cached, can be abs or relative
+			cachePath: defaultCachePath,
+			// whether cache usage messages should be output
+			nosiyCache: false
+		},
+		loaderUtils.getOptions(this)
+	)
+	const cache_path = path.isAbsolute(options.cachePath) ? options.cachePath : path.join(process.cwd(), options.cachePath)
 	var resolve = this.resolve;
 	var addDependency = this.addDependency;
-	var emitWarning = this.emitWarning || function() {};
+	var emitWarning = this.emitWarning || function () { };
 	var match = input.match(regex1) || input.match(regex2);
-	if(match) {
+	if (match) {
 		var url = match[1];
 		var dataUrlMatch = regexDataUrl.exec(url);
 		var callback = this.async();
-		if(dataUrlMatch) {
+		if (dataUrlMatch) {
 			var mapBase64 = dataUrlMatch[1];
 			var mapStr = (new Buffer(mapBase64, "base64")).toString();
 			var map;
@@ -46,18 +58,18 @@ module.exports = function(input, inputMap) {
 				map = JSON.parse(mapStr)
 			} catch (e) {
 				emitWarning("Cannot parse inline SourceMap '" + mapBase64.substr(0, 50) + "': " + e);
-				return untouched();				
+				return untouched();
 			}
 			processMap(map, this.context, callback);
 		} else {
-			resolve(this.context, loaderUtils.urlToRequest(url), function(err, result) {
-				if(err) {
+			resolve(this.context, loaderUtils.urlToRequest(url), function (err, result) {
+				if (err) {
 					emitWarning("Cannot find SourceMap '" + url + "': " + err);
 					return untouched();
 				}
 				addDependency(result);
-				fs.readFile(result, "utf-8", function(err, content) {
-					if(err) {
+				fs.readFile(result, "utf-8", function (err, content) {
+					if (err) {
 						emitWarning("Cannot open SourceMap '" + result + "': " + err);
 						return untouched();
 					}
@@ -73,61 +85,80 @@ module.exports = function(input, inputMap) {
 	function untouched() {
 		callback(null, input, inputMap);
 	}
-    function processMap(map, context, callback) {
-	if(!map.sourcesContent || map.sourcesContent.length < map.sources.length) {
-	    var sourcePrefix = map.sourceRoot ? map.sourceRoot + "/" : "";
-	    map.sources = map.sources.map(function(s) {
-                if(s.startsWith("http")||s.startsWith("file")) return s;
-                return sourcePrefix + s;
-            });
-	    delete map.sourceRoot;
-	    var missingSources = map.sourcesContent ? map.sources.slice(map.sourcesContent.length) : map.sources;
-	    async.map(missingSources, function(source, callback) {
-                // source is the source "url" file://,http:// or a plain OS file path that needs resolving
-                if(source.startsWith("http")) {
-                    if(!!!options.bundleHttp) callback(null, null)
-                    else {
-                        fetch(source)
-                            .then(resp => {
-                                if(resp.ok) {
-                                    resp.text().then(content =>  {
-                                        addDependency(source)
-                                        callback(null, {
-                                            source: source,
-                                            content: content,
-                                        })
-                                    })
-                                } else callback(null, null)
-                            })
-                            .catch(error => {
-                                emitWarning("Cannot fetch remote resource '" + source + "': "+ error)
-                                callback(null, null)
-                            })
-                    }
-                }
-                        else if(source.startsWith("file://")) {
-                            const localsource = source.slice(7)
-                            fs.readFile(localsource, "utf-8", function(err, content) {
-                                if(err) {
-                                    if(!!!options.skipFileURLWarnings)
-                                        emitWarning("Could not open source file '" + source + "': " + err)
-                                    return callback(null, null)
-                                }
-                                addDependency(source)
-                                callback(null, {
-                                    source: source,
-                                    content: content
-                                })
-                            })
-                        }
-			else resolve(context, loaderUtils.urlToRequest(source), function(err, result) {
-					if(err) {
+	function processMap(map, context, callback) {
+		if (!map.sourcesContent || map.sourcesContent.length < map.sources.length) {
+			var sourcePrefix = map.sourceRoot ? map.sourceRoot + "/" : "";
+			map.sources = map.sources.map(function (s) {
+				if (s.startsWith("http") || s.startsWith("file") || path.isAbsolute(s)) return s;
+				return path.join(sourcePrefix, s);
+			});
+			delete map.sourceRoot;
+			var missingSources = map.sourcesContent ? map.sources.slice(map.sourcesContent.length) : map.sources;
+			async.map(missingSources, function (source, callback) {
+				// source is the source "url" file://,http:// or a plain OS file path that needs resolving
+				if (source.startsWith("http")) {
+					// check local cache for fetched data so we don't have to refetch
+					const filename = url_parse(source).pathname
+					const cache_content_path = path.join(cache_path, filename)
+					if (fs.existsSync(cache_content_path)) {
+						// it exists, return it
+						if (options.noisyCache) console.log("Cache hit for ", source)
+						addDependency(source)
+						callback(null, {
+							source,
+							content: fs.readFileSync(cache_content_path, "utf8")
+						})
+					}
+					else if (!!!options.bundleHttp) callback(null, null)
+					else {
+						// perform expensive fetch
+						fetch(source)
+							.then(resp => {
+								if (resp.ok) {
+									resp.text().then(content => {
+										// save cache entry
+										if (options.noisyCache) console.log("Saving cache content")
+										fs.mkdirSync(path.join(cache_content_path, ".."), { recursive: true })
+										fs.writeFileSync(cache_content_path, content, "utf8")
+										addDependency(source)
+										callback(null, {
+											source: source,
+											content: content,
+										})
+									})
+								} else callback(null, null)
+							})
+							.catch(error => {
+								emitWarning("Cannot fetch remote resource '" + source + "': " + error)
+								callback(null, null)
+							})
+					}
+				}
+				else if (source.startsWith("file://") || path.isAbsolute(source)) {
+					//console.log("file source map", source)
+					const localsource = path.isAbsolute(source) ? source : source.slice(7)
+					fs.readFile(localsource, "utf-8", function (err, content) {
+						if (err) {
+							if (!!!options.skipFileURLWarnings)
+								emitWarning("Could not open source file '" + source + "': " + err)
+							return callback(null, null)
+						}
+						addDependency(source)
+						callback(null, {
+							source: source,
+							content: content
+						})
+					})
+				}
+				// resolve via webpack methods
+				else resolve(context, loaderUtils.urlToRequest(source), function (err, result) {
+					if (err) {
 						emitWarning("Cannot find source file '" + source + "': " + err);
 						return callback(null, null);
 					}
 					addDependency(result);
-					fs.readFile(result, "utf-8", function(err, content) {
-						if(err) {
+					fs.readFile(result, "utf-8", function (err, content) {
+						if (err) {
 							emitWarning("Cannot open source file '" + result + "': " + err);
 							return callback(null, null);
 						}
@@ -137,10 +168,10 @@ module.exports = function(input, inputMap) {
 						});
 					});
 				});
-			}, function(err, info) {
+			}, function (err, info) {
 				map.sourcesContent = map.sourcesContent || [];
-				info.forEach(function(res) {
-					if(res) {
+				info.forEach(function (res) {
+					if (res) {
 						map.sources[map.sourcesContent.length] = res.source;
 						map.sourcesContent.push(res.content);
 					} else {
